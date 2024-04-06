ddf2235f9b5a51741ebe1eb83ce7cdc7ed1fa050
Pradhumn2 <pradhumnsharma2002@gmail.com>
2024-04-06 20:20:26 +0530

    updated

--- server.js
+++ server.js
@@ -36,8 +36,14 @@ connectDB();
 const app = express(); //All functionality of express gets stored in app
 
 //middlewares
-app.use(express.json());
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(cors(corsOptions));
+app.use(express.json());
 app.use(morgan("dev"));
 
 //routes
