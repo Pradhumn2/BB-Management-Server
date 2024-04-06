f6f9498602684869471e6f527b735f77a78c65e1
Pradhumn2 <pradhumnsharma2002@gmail.com>
2024-04-06 20:16:12 +0530

    updated

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ const allowedOrigins = [
 
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log(origin);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
